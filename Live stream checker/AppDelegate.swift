605f7fd53dd6da27c584f2b67fbd7bd88fc798a0
igor25 <is246@cornell.edu>
2016-10-16 05:39:26 -0700

    Initial Commit

---
+++ Live stream checker/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Live stream checker
+//
+//  Created by Igor Segota on 10/16/16.
+//  Copyright © 2016 my.companyIgor Segota. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
